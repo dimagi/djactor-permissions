7a638de6647d7bbfaca1fcb695abd28d127feb58
dmyung <dmyung@dimagi.com>
2011-08-06 17:10:08 -0400

    adding a doc_id property for storing a couchdoc

--- permissions/models.py
+++ permissions/models.py
@@ -49,6 +49,7 @@ class Actor(models.Model):
     A user can have multiple actors for it, each having a unique set of roles given to it.
     """
     id = models.CharField(max_length=32, unique=True, default=make_uuid, primary_key=True, editable=False)
+    doc_id = models.CharField(max_length=32, unique=True, null=True, db_index=True)
 
     name = models.CharField(_("Actor Name"), max_length=160, unique=True)
     user = models.ForeignKey(User, null=True, blank=True)
